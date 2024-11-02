ec8393e683541bf09867994378a1da2d812039ce
Daniil <daniil.shabarov@github.com>
2024-11-02 14:14:19 +0300

    Init

---
+++ src/test/java/com/example/mdp/MdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
